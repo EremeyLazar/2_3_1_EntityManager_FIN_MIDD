28229a04219eafbd784b7caadc26684e4a8a8998
Eremey Lazar <elazar1926@gmail.com>
2023-11-19 18:01:21 +0300

    2

---
+++ src/main/java/model/Cars.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Cars {
+
+    private String owner;
+    private String model;
+    private int number;
+
+    public Cars() {
+    }
+
+    public Cars(String owner, String model, int number) {
+        this.owner = owner;
+        this.model = model;
+        this.number = number;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import model.Cars;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,4 +18,11 @@ public class CarsController {
         model.addAttribute("messages", messages);
         return "cars";
     }
+
+    Cars car1 = new Cars("Viktor", "Mustang", 00165);
+    Cars car2 = new Cars("Ignat", "Mustang", 21453);
+    Cars car3 = new Cars("Georgiy", "Mustang", 63002);
+    Cars car4 = new Cars("James", "Mustang", 00007);
+    Cars car5 = new Cars("Kimanilau", "Mustang", 12210);
+
 }
