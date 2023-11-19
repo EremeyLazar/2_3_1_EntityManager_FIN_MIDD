9d9223a1c071a5aba12483bdbe07b1cb415115df
Eremey Lazar <elazar1926@gmail.com>
2023-11-19 17:27:15 +0300

    1

---
+++ src/main/java/web/controller/CarsController.java
@@ -0,0 +1,20 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarsController {
+
+    @GetMapping(value = "/cars")
+    public String printCars(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Here will be cars");
+        model.addAttribute("messages", messages);
+        return "cars";
+    }
+}
