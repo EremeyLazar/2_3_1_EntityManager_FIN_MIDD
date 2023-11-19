d616d2fdf702a562031c3525e17b34aa880de577
Eremey Lazar <elazar1926@gmail.com>
2023-11-19 18:08:49 +0300

    3

---
+++ src/main/java/service/ServiceImp.java
@@ -0,0 +1,11 @@
+package service;
+
+import model.Cars;
+
+public class ServiceImp {
+    Cars car1 = new Cars("Viktor", "Mustang", 00165);
+    Cars car2 = new Cars("Ignat", "Mustang", 21453);
+    Cars car3 = new Cars("Georgiy", "Mustang", 63002);
+    Cars car4 = new Cars("James", "Mustang", 00007);
+    Cars car5 = new Cars("Kimanilau", "Mustang", 12210);
+}
