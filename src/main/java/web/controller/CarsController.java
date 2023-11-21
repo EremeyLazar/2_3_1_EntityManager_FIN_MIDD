d224ae2e291bd538a70eeb18e69c05db49600684
Eremey Lazar <elazar1926@gmail.com>
2023-11-21 17:48:09 +0300

    6

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import service.Service;
 import service.ServiceImp;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
