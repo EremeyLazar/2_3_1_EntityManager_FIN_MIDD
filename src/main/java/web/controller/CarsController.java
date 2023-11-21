a84e4da9791e3d7888df416caca042de09dc6de7
Eremey Lazar <elazar1926@gmail.com>
2023-11-21 17:46:16 +0300

    @RequestParam

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -3,6 +3,7 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import service.Service;
 import service.ServiceImp;
 
@@ -15,9 +16,24 @@ public class CarsController {
     Service service = new ServiceImp();
 
 
+//    @GetMapping(value = "/cars")
+//    public String showCars(HttpServletRequest request, ModelMap model) {
+//        Integer carsNum = Integer.parseInt(request.getParameter("count"));
+//
+//        if (carsNum > 5) {
+//            carsNum = 5;
+//        }
+//
+//        List<String> messages = new ArrayList<>();
+//        messages.add("Please, see the list of " + carsNum + " cars.");
+//        service.getCarsFromList(carsNum).stream().forEach(x -> messages.add(String.valueOf(x)));
+//        model.addAttribute("messages", messages);
+//
+//        return "cars";
+//    }
+
     @GetMapping(value = "/cars")
-    public String showCars(HttpServletRequest request, ModelMap model) {
-        Integer carsNum = Integer.parseInt(request.getParameter("count"));
+    public String showCars(@RequestParam("count") int carsNum, ModelMap model) {
 
         if (carsNum > 5) {
             carsNum = 5;
