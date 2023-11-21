11423693db84e2556fa8dec97b1644b0ff727570
Eremey Lazar <elazar1926@gmail.com>
2023-11-21 17:47:55 +0300

    5

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -15,23 +15,6 @@ import java.util.List;
 public class CarsController {
     Service service = new ServiceImp();
 
-
-//    @GetMapping(value = "/cars")
-//    public String showCars(HttpServletRequest request, ModelMap model) {
-//        Integer carsNum = Integer.parseInt(request.getParameter("count"));
-//
-//        if (carsNum > 5) {
-//            carsNum = 5;
-//        }
-//
-//        List<String> messages = new ArrayList<>();
-//        messages.add("Please, see the list of " + carsNum + " cars.");
-//        service.getCarsFromList(carsNum).stream().forEach(x -> messages.add(String.valueOf(x)));
-//        model.addAttribute("messages", messages);
-//
-//        return "cars";
-//    }
-
     @GetMapping(value = "/cars")
     public String showCars(@RequestParam("count") int carsNum, ModelMap model) {
 
